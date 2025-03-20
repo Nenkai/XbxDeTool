f008e04673d532cdb51f28734343d59056f12cf2
Nenkai <Nenkai@users.noreply.github.com>
2025-03-20 21:22:49 +0100

    bump version

--- XbxDeTool/Program.cs
+++ XbxDeTool/Program.cs
@@ -20,7 +20,7 @@ public class Program
     private static ILoggerFactory _loggerFactory;
     private static Microsoft.Extensions.Logging.ILogger _logger;
 
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 
     static async Task<int> Main(string[] args)
     {
